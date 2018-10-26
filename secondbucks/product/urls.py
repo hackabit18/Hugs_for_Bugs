e8e86504e9a70b56eaca1a5772a4dcd70af56739
AKash Kumar Shrivastva <akash.shrivastva@gmail.com>
2018-10-26 23:21:33 +0530

    made base and index page

--- secondbucks/product/urls.py
+++ secondbucks/product/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     #/
     url('^$', views.index, name='index'),
+
 ]
--- secondbucks/product/views.py
+++ secondbucks/product/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from django.shortcuts import render, redirect
 
 def index(request):
-    return HttpResponse("Working")
+    if request.method == 'POST':
+        college_id = request.POST.get("college_name")
+        return redirect('product:categories', college_id=college_id)
+
+    colleges = College.objects.all()
+    return render(request, 'index.html', {'colleges': colleges})

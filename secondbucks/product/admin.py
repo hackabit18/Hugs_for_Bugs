c22395a04ca8ae360e1510a1164a9272ddf34eb2
nikunjagarwal321 <nikunjagaral321@gmail.com>
2018-10-27 11:10:28 -0700

    Views for login registration created

--- secondbucks/product/admin.py
+++ secondbucks/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category, College, Product, UserDetails,Wisher
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(College)
+admin.site.register(Product)
+admin.site.register(UserDetails)
+admin.site.register(Wisher)
--- secondbucks/product/views.py
+++ secondbucks/product/views.py
@@ -104,16 +104,84 @@ def RegisterShareProduct(request):
 
 
 
+def RegisterUserDetails(request):
+    form = UserDetailsForm()
+
+    if request.method == 'GET':
+        return render(request, 'userdet_form.html' , {'form': form})
+    
+    if request.method == 'POST':
+        form = UserDetailsForm(request.POST)
+        if form.is_valid():
+            temp = UserDetails()
+            temp.user = request.user
+            temp.college = form.cleaned_data['college']
+            temp.phone = form.cleaned_data['phone']
+            temp.img = form.cleaned_data['img']
+            temp.save() 
+            return HttpResponse("Done")
+        else:
+            return HttpResponse(form.errors)
+
+#not  working
+class UpdateUserDetails(UpdateView):
+    model = UserDetails
+    fields = ['college', 'phone', 'img']
+
+
+class DeleteProduct(DeleteView):
+    model = Product
+    success_url = reverse_lazy('product:index')
 
 
 
+class UserFormView(View):
+    form_class = UserSignupForm
+    template_name = 'user_reg_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+ 
+            user=authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return redirect('product:register-userdetails')
+
+        return render(request, self.template_name, {'form': form})
 
 
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('product:index')
+            else:
+                return render(request, 'login_form.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'login_form.html', {'error_message': 'Invalid login'})
+    return render(request, 'login_form.html')
 
 
+def logout_user(request):
+    logout(request)
+    return redirect('product:index')
 
 
-
-
-def index(request):
-    return HttpResponse("Working")

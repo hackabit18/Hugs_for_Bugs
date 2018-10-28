2be535bed7495ac3f822b0676b8f6bda8bf10ec4
nikunjagarwal321 <nikunjagaral321@gmail.com>
2018-10-27 17:52:21 -0700

    Views for email done

--- secondbucks/product/urls.py
+++ secondbucks/product/urls.py
@@ -3,6 +3,33 @@ from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #/
     url('^$', views.index, name='index'),
+
+    #/college_name/categories
+    url('^(?P<college_id>[0-9]+)/categories/$', views.category_list, name='categories'),
+
+    #/college_name/category_name
+    url('^(?P<college_id>[0-9]+)/(?P<category_id>[0-9]+)/$', views.category_wise, name='category_wise'),
+
+    #/college_name/category_name/product_name/
+    url('^(?P<college_id>[0-9]+)/(?P<category_id>[0-9]+)/(?P<product_id>[0-9]+)/$', views.product_details, name='product_details'),
+
+    #/college/register/
+    url('^college/register/$',views.RegisterCollege.as_view(),name='college-add'),
+
+    #/product/add/                                                           
+    url('^product/add/$',views.add_product, name='product-add'),
+
+    #/product/add/sell/                                                            
+    url('^product/add/sell/$',views.RegisterSellProduct,name='product-add-sell'),
+
+    #/product/add/share/                                                           
+    url('^product/add/share/$',views.RegisterShareProduct,name='product-add-share'),
+
+    #register/user/
+    url('^register/user/$',views.UserFormView.as_view(),name='register-user'),
+
+    #register/user_id/userdetails/
+    url('^register/userdetails/$',views.RegisterUserDetails,name='register-userdetails'),
+
 ]
--- secondbucks/product/views.py
+++ secondbucks/product/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Category, College, Product, UserDetails, Wisher, ReportedAd
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View
+from django.contrib.auth.models import User
+from .forms import UserSignupForm, UserDetailsForm, SellProductRegistrationForm, ShareProductRegistrationForm
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from .serializers import CollegeSerializer
 
 
 def index(request):
     if request.method == 'POST':
-        college_id = request.POST.get("college_name")
-        return redirect('product:categories', college_id=college_id)
+        college_name = request.POST.get("myCountry")
+        college_id = College.objects.get(name=college_name)
+        return redirect('product:categories', college_id=college_id.pk)
 
     colleges = College.objects.all()
     return render(request, 'index.html', {'colleges': colleges})
@@ -40,7 +50,6 @@ class RegisterCollege(CreateView):
 def add_product(request):
     return render(request, 'product/add_product.html')
 
-
 def RegisterSellProduct(request):
     form = SellProductRegistrationForm()
 
@@ -51,6 +60,7 @@ def RegisterSellProduct(request):
         form = SellProductRegistrationForm(request.POST)
         if form.is_valid():
             temp = Product()
+            temp.img = form.cleaned_data['img']
             temp.name = form.cleaned_data['name']
             temp.descripton = form.cleaned_data['descripton']
             temp.category = form.cleaned_data['category']
@@ -70,7 +80,7 @@ def RegisterSellProduct(request):
             return redirect('product:product_details', college_id = temp.college.pk,category_id=temp.category.pk,product_id=temp.pk)
         
         else:
-            return HttpResponse(form.errors)
+            return HttpResponse(form['img'].errors)
 
 def RegisterShareProduct(request):
     form = ShareProductRegistrationForm()
@@ -86,6 +96,7 @@ def RegisterShareProduct(request):
             temp.descripton = form.cleaned_data['descripton']
             temp.category = form.cleaned_data['category']
             temp.college = form.cleaned_data['college']
+            temp.img = form.cleaned_data['img']
             temp.no_of_days = form.cleaned_data['no_of_days']
             temp.user = request.user
             temp.save()
@@ -106,10 +117,15 @@ def RegisterShareProduct(request):
 
 def RegisterUserDetails(request):
     form = UserDetailsForm()
-
     if request.method == 'GET':
-        return render(request, 'userdet_form.html' , {'form': form})
-    
+        curr_user = request.user
+        try:
+            u = UserDetails.objects.get(user=curr_user)
+            return HttpResponse("You have already registered. You can Update previous Details")
+
+        except UserDetails.DoesNotExist:
+            return render(request, 'userdet_form.html' , {'form': form})
+            
     if request.method == 'POST':
         form = UserDetailsForm(request.POST)
         if form.is_valid():
@@ -119,7 +135,7 @@ def RegisterUserDetails(request):
             temp.phone = form.cleaned_data['phone']
             temp.img = form.cleaned_data['img']
             temp.save() 
-            return HttpResponse("Done")
+            return redirect('product:categories', college_id=temp.college.pk )
         else:
             return HttpResponse(form.errors)
 
@@ -185,3 +201,32 @@ def logout_user(request):
     return redirect('product:index')
 
 
+def ads(request):
+    current_user = request.user
+    ad = Product.objects.filter(user=current_user)
+    return render(request, 'product/ads.html', {'ads': ad})
+
+@login_required
+def notify(request, college_id, category_id):
+    wisher = Wisher()
+    wisher.college=College.objects.get(pk=college_id)
+    wisher.category=Category.objects.get(pk=category_id)
+    wisher.user = request.user
+    wisher.save()
+    return render(request, 'notified.html', {'category': wisher.category} )
+
+@login_required
+def report(request, product_id):
+    ad = ReportedAd()
+    ad.product=Product.objects.get(pk=product_id)
+    ad.user = request.user
+    ad.save()
+    return  render(request, 'report.html')
+
+def view_user(request, user_id):
+    u = User.objects.get(pk=user_id)
+    try:
+        user_det = UserDetails.objects.get(user=u)
+    except UserDetails.DoesNotExist:
+        user_det = None
+    return render(request, 'view_user_details.html',{'userdetail': user_det, 'u': u})

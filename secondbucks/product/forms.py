4c0c4f1ac9ba0761086368893983a7a706f6589a
Akash Shrivastva <akash.shrivastva@gmail.com>
2020-05-18 21:16:07 +0530

    haha

--- secondbucks/product/forms.py
+++ secondbucks/product/forms.py
@@ -15,11 +15,7 @@ class UserSignupForm(forms.ModelForm):
 class UserDetailsForm(forms.ModelForm):
     class Meta:
         model=UserDetails
-<<<<<<< HEAD
         fields=['state','phone','img']
-=======
-        fields=['college','phone','img']
->>>>>>> 79291096917f6b07413c822b2fd3e8452f130729
 
 
 class SellProductRegistrationForm(forms.ModelForm):
